4ab08a5e04d2b50a9e3f6e888314068bc0940bb0
Luis Kala <luisangelkala@gmail.com>
2026-03-16 13:49:13 -0500

    Update config inicial

--- __manifest__.py
+++ __manifest__.py
@@ -18,6 +18,16 @@
         'views/container_type_views.xml',
         'report/shipping_reports.xml',
     ],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'data/sequence.xml',
+        'data/container_data.xml',
+        'views/shipping_management_views.xml', 
+        'views/container_type_views.xml',
+        # TERCERO: Los reportes (déjalos comentados si aún no hemos aplicado la solución del Error #003)
+        # 'report/shipping_reports.xml',
+    ],
     'application': True,
     'installable': True,
     'license': 'LGPL-3',
