6eaf86be6ccf1b9f6349da2daefca68aae918884
Luis Kala <luisangelkala@gmail.com>
2026-03-19 06:24:14 -0500

    Add new features HBL and Classifiers

--- models/shipping_line.py
+++ models/shipping_line.py
@@ -34,8 +34,8 @@ class ShippingManagementLine(models.Model):
                 qr = qrcode.QRCode(
                     version=1,
                     error_correction=qrcode.constants.ERROR_CORRECT_L,
-                    box_size=10,
-                    border=4,
+                    box_size=6,
+                    border=1,
                 )
                 qr.add_data(line.package_code)
                 qr.make(fit=True)
