7f2681cd0c8a6354d5ee3f3290bb80d03837e168
Luis Kala <luisangelkala@gmail.com>
2026-03-19 06:08:24 -0500

    Add new features HBL and Classifiers

--- models/shipping_line.py
+++ models/shipping_line.py
@@ -1,4 +1,7 @@
 from odoo import models, fields, api, _
+import qrcode
+import base64
+from io import BytesIO
 
 class ShippingManagementLine(models.Model):
     _name = 'shipping.management.line'
@@ -13,9 +16,37 @@ class ShippingManagementLine(models.Model):
     sender_id = fields.Many2one('res.partner', string='Remitente', required=True)
     receiver_id = fields.Many2one('res.partner', string='Destinatario', required=True)
     
+    shipping_type = fields.Selection([
+        ('envio', 'Envío'),
+        ('ena', 'ENA')
+    ], string='Tipo de Envío', required=True, default='envio')
+
     # Código de bulto con valor temporal. Se asigna al confirmar el envío.
     package_code = fields.Char(string='Código Paquete', default=lambda self: self.env['ir.sequence'].next_by_code('shipping.management'), readonly=True, copy=False)
     
+    # QR Code generado en backend para alta calidad en PDF
+    qr_image = fields.Binary(string="QR Code", compute="_compute_qr_image", store=True)
+
+    @api.depends('package_code')
+    def _compute_qr_image(self):
+        for line in self:
+            if line.package_code:
+                qr = qrcode.QRCode(
+                    version=1,
+                    error_correction=qrcode.constants.ERROR_CORRECT_L,
+                    box_size=10,
+                    border=4,
+                )
+                qr.add_data(line.package_code)
+                qr.make(fit=True)
+                img = qr.make_image(fill_color="black", back_color="white")
+                temp = BytesIO()
+                img.save(temp, format="PNG")
+                qr_image = base64.b64encode(temp.getvalue())
+                line.qr_image = qr_image
+            else:
+                line.qr_image = False
+
     # Nuevos campos de detalle de carga
     description = fields.Char(string='Mercancía')
     packages_qty = fields.Integer(string='Cantidad Bultos', default=1)
